432d9606eae78d05b0a35168f95644415c8bfe2f
jaxp2 <spx0169x@gmail.com>
2022-07-29 11:36:41 +0100

    Add files via upload

--- cogs/help.py
+++ cogs/help.py
@@ -46,11 +46,10 @@ class Help(commands.Cog):
 
             cogs_desc = ''
             for cog in self.bot.cogs:
-                if cog != "Startup" or cog != "Errors":
+                if not "Errors" in cog and not 'Startup' in cog:
                     cogs_desc += f'`{cog}` {self.bot.cogs[cog].__doc__}\n'
 
             emb.add_field(name='Modules', value=cogs_desc, inline=False)
-            emb.set_footer(text=f'Requested by {ctx.author.name}', icon_url=ctx.author.avatar_url)
             commands_desc = ''
             for command in self.bot.walk_commands():
                 if not command.cog_name and not command.hidden:
@@ -84,6 +83,7 @@ class Help(commands.Cog):
                                                 description=f"I've never heard from a module/command called `{module}` before :scream:",
                                                 color=error_color)
 
+        emb.set_footer(text=f'Requested by {ctx.author.name}', icon_url=ctx.author.avatar_url)
         await send_embed(ctx, emb)
 def setup(bot):
     bot.add_cog(Help(bot))

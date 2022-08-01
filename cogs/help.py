25ebcad9c2f269559ee2d387e3f22849728606c6
jaxp2 <spx0169x@gmail.com>
2022-08-01 14:45:53 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -58,7 +58,7 @@ class Help(commands.Cog):
                 emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
 
             emb.add_field(name="About", value=f"\n\
-                                    {bot_name} is maintained by `nox#4041`")
+                                    {bot_name} is maintained by `CriticRay#5008`")
         else:
             for cog in self.bot.cogs:
                 if cog.lower() == module.lower():

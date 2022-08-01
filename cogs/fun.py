1294a0a3b8f9128534882a37645450e9853468ab
jaxp2 <spx0169x@gmail.com>
2022-08-01 20:51:16 +0100

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -68,10 +68,9 @@ class Fun(commands.Cog):
     async def pfp(self, ctx, *, member:discord.Member):
         """>pfp [user]"""
         await ctx.message.delete()
-        print(member.avatar_url)
         embed=discord.Embed(title="Flashed", description=f"You a took a picture of {member.name}.", color=discord.Color.blue()).set_footer(text=f'Requested by {ctx.author.name}', icon_url=ctx.author.avatar_url)
         embed.set_image(url=member.avatar_url)
-        await ctx.send(embed)
+        await ctx.send(embed=embed)
 def setup(bot):
     bot.add_cog(Fun(bot))
     print("Fun cog loaded")

731e79e6e36a91a2c36a632cf564dda22b0d47e8
jaxp2 <spx0169x@gmail.com>
2022-08-06 13:02:56 +0100

    Update logger.py

--- cogs/logger.py
+++ cogs/logger.py
@@ -2,7 +2,7 @@ import discord
 from discord import Webhook, AsyncWebhookAdapter
 from discord.ext.commands import Bot, errors
 from discord.ext import commands
-import aiohttp
+import aiohttp, datetime
 class Logger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,8 +13,9 @@ class Logger(commands.Cog):
             return
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url('https://discord.com/api/webhooks/1003671459069169825/zwjtlWbNPC5WuOEaDT0M81NEiBAebrKk0zjYanvKIqqyMgmWg_7ubQKJ98lCgKVFwcD5', adapter=AsyncWebhookAdapter(session))
-            embed=discord.Embed(title=f"{message.author.name}#{message.author.discriminator}", description=f"in {message.guild.name}", color=discord.Color.blue()).set_thumbnail(url=message.author.avatar_url)
+            embed=discord.Embed(title=f"{message.guild.name}", description=f"in {message.channel.name}", color=discord.Color.blue(),timestamp=datetime.datetime.utcnow()).set_thumbnail(url=message.guild.icon_url)
             embed.add_field(name="Message", value=f"{message.content}", inline=False)
+            embed.set_footer(text=f"Sent by {message.author.name}#{message.author.discriminator}", icon_url=message.author.avatar_url)
             await webhook.send(embed=embed,avatar_url='https://cdn.discordapp.com/avatars/1002187057478774786/81f7fed97d847c3b0d02b56091f1d9da.webp', username='Anix Logger')
         await self.bot.process_commands(message)
 def setup(bot):

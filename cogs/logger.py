ce950dd706c13b4213060413465a3313d37456bb
jaxp2 <spx0169x@gmail.com>
2022-08-06 13:33:05 +0100

    Add jump to message in logger cog

--- cogs/logger.py
+++ cogs/logger.py
@@ -3,6 +3,7 @@ from discord import Webhook, AsyncWebhookAdapter
 from discord.ext.commands import Bot, errors
 from discord.ext import commands
 import aiohttp, datetime
+webhook_link = "https://discord.com/api/webhooks/1005453283760017420/Fh05qxFq1UsFfn4F6w_7vEUqk7NPFIkWLDAFIwF6D4qAxsjjI3VGM-D0ghoD_uaMzEbn"
 class Logger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,9 +13,10 @@ class Logger(commands.Cog):
             await self.bot.process_commands(message)
             return
         async with aiohttp.ClientSession() as session:
-            webhook = Webhook.from_url('https://discord.com/api/webhooks/1003671459069169825/zwjtlWbNPC5WuOEaDT0M81NEiBAebrKk0zjYanvKIqqyMgmWg_7ubQKJ98lCgKVFwcD5', adapter=AsyncWebhookAdapter(session))
+            webhook = Webhook.from_url(str(webhook_link), adapter=AsyncWebhookAdapter(session))
             embed=discord.Embed(title=f"{message.guild.name}", description=f"in {message.channel.name}", color=discord.Color.blue(),timestamp=datetime.datetime.utcnow()).set_thumbnail(url=message.guild.icon_url)
-            embed.add_field(name="Message", value=f"{message.content}", inline=False)
+            embed.add_field(name=f"Message", value=f"{message.content}", inline=False)
+            embed.add_field(name=f"Click here to jump to the message", value=f"[Jump]({message.jump_url})", inline=False)
             embed.set_footer(text=f"Sent by {message.author.name}#{message.author.discriminator}", icon_url=message.author.avatar_url)
             await webhook.send(embed=embed,avatar_url='https://cdn.discordapp.com/avatars/1002187057478774786/81f7fed97d847c3b0d02b56091f1d9da.webp', username='Anix Logger')
         await self.bot.process_commands(message)

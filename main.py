f4af82718eb2413d00cb3ed5b507c6b50f0d671a
jaxp2 <spx0169x@gmail.com>
2022-08-06 14:30:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
     
-bot.run(os.environ.get('IS_HEROKU', None))
+bot.run(os.environ.get('TOKEN', None))

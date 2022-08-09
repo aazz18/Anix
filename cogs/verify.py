235b0add101eb143dd61c821c2d87c39c59e1eb1
jaxp2 <spx0169x@gmail.com>
2022-08-09 20:45:30 +0100

    Update verify.py

--- cogs/verify.py
+++ cogs/verify.py
@@ -15,7 +15,7 @@ class Verify(commands.Cog):
             welcomeid = 1002659844353970320
         self.key = random.randint(100000, 999999)
         await self.bot.get_channel(welcomeid).send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description=f"Welcome to the {member.guild.name}! Please verify yourself with the command `verify` in the {member.guild.name} server. If you have any questions, please ask a staff member. Have fun!", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif").set_author(name="Done", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/checkmark-green.png?token=GHSAT0AAAAAABV3GFY562BKGYAAD6XVUZCYYXSUEMA"))
-        await member.send(f"<@{member.id}> Welcome to the {member.guild.name}! Please verify yourself with the command `verify {self.key}` in the {member.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
+        await member.send(f"<@{member.id}> Welcome to the {member.guild.name}! Please verify yourself with the command `>verify {self.key}` in the {member.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
     @commands.Cog.listener()
     async def on_message(self, message):
         while True:
@@ -23,7 +23,7 @@ class Verify(commands.Cog):
                 return
             if message.channel.id == 1006614884718485555:
                 verifyRole = discord.utils.get(message.guild.roles, name="Verified")
-                if self.key in message.content:
+                if str(self.key) in message.content:
                     if verifyRole in message.author.roles:
                         await message.author.send(f"<@{message.author.id}> You are already verified!")
                         await self.bot.process_commands(message)
@@ -35,7 +35,7 @@ class Verify(commands.Cog):
                 else:
                     await message.author.send(f"<@{message.author.id}> Invalid key. Please try again.")
                     self.key = random.randint(100000, 999999)
-                    await message.author.send(f"<@{message.author.id}> Your new verification key is `{self.key}`. Please verify yourself with the command `verify {self.key}` in the {message.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
+                    await message.author.send(f"<@{message.author.id}> Your new verification key is `{self.key}`. Please verify yourself with the command `>verify {self.key}` in the {message.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
 
 def setup(bot):
     bot.add_cog(Verify(bot))

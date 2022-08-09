23bfd53c7f26e62d390c904846714e1527969c2f
jaxp2 <spx0169x@gmail.com>
2022-08-09 18:51:05 +0100

    Added verfication system

--- cogs/startup.py
+++ cogs/startup.py
@@ -4,22 +4,13 @@ from discord.ext import commands
 class Startup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         print('Logged in as: ' + self.bot.user.name + '#' + self.bot.user.discriminator + ' - (' + str(self.bot.user.id) + ')')
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         print(f'Left guild: {guild.name} - {str(guild.id)}')
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        print(f'Guild - {member.guild.name} - {str(member.guild.id)} -  {member.name} - {str(member.id)} joined :)') 
-        if member.guild.id == 993573362221715546:
-            welcomeid = 993573959062790154
-        elif member.guild.id == 1002659843720617995:
-            welcomeid = 1002659844353970320
-        await self.bot.get_channel(welcomeid).send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description="Please read the rules and have fun!", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif"))
-        await member.send(f"<@{member.id}> Welcome to the {member.guild.name}! Please read the rules and have fun!")
 def setup(bot):
     bot.add_cog(Startup(bot))
     print("Startup cog loaded")
---
+++ cogs/verify.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import random
+class Verify(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.key = None
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f'Guild - {member.guild.name} - {str(member.guild.id)} -  {member.name} - {str(member.id)} joined :)') 
+        if member.guild.id == 1006599826315685888:
+            welcomeid = 1006604054643875911
+        elif member.guild.id == 1002659843720617995:
+            welcomeid = 1002659844353970320
+        self.key = random.randint(100000, 999999)
+        await self.bot.get_channel(welcomeid).send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description=f"Welcome to the {member.guild.name}! Please verify yourself with the command `verify` in the {member.guild.name} server. If you have any questions, please ask a staff member. Have fun!", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif").set_author(name="Done", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/checkmark-green.png?token=GHSAT0AAAAAABV3GFY562BKGYAAD6XVUZCYYXSUEMA"))
+        await member.send(f"<@{member.id}> Welcome to the {member.guild.name}! Please verify yourself with the command `verify {self.key}` in the {member.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        while True:
+            await message.delete()
+            if message.author.bot:
+                return
+            if message.channel.id == 1006614884718485555:
+                await message.channel.purge(limit=1)
+                verifyRole = discord.utils.get(message.guild.roles, name="Verified")
+                if message.content == self.key:
+                    if verifyRole in message.author.roles:
+                        await message.author.send(embed=discord.Embed(description=f"You are already verified.", color=discord.Color.red()).set_footer(text=f"Requested by {message.author.name}", icon_url=message.author.avatar_url).set_author(name="Error", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/crossmark-red.png?token=GHSAT0AAAAAABV3GFY4J4T3G6OGZTZLGGXQYXOQENQ"))
+                        break
+                    await message.author.add_roles(verifyRole)
+                    await message.author.send(embed=discord.Embed(description=f"You have been verified.", color=discord.Color.green()).set_footer(text=f"Requested by {message.author.name}", icon_url=message.author.avatar_url).set_author(name="Done", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/checkmark-green.png?token=GHSAT0AAAAAABV3GFY562BKGYAAD6XVUZCYYXSUEMA"))
+                    break
+                else:
+                    await message.author.send(embed=discord.Embed(description=f"You have entered an invalid key.", color=discord.Color.red()).set_footer(text=f"Requested by {message.author.name}", icon_url=message.author.avatar_url).set_author(name="Error", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/crossmark-red.png?token=GHSAT0AAAAAABV3GFY4J4T3G6OGZTZLGGXQYXOQENQ"))
+                    self.key = random.randint(100000, 999999)
+                    await message.author.send(f"<@{message.author.id}> Your new verification key is `{self.key}`. Please verify yourself with the command `verify {self.key}` in the {message.guild.name} server. If you have any questions, please ask a staff member. Have fun!")
+

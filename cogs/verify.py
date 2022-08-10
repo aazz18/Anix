8906414bdc4a999c87ec7a7bc7c0afb4f4d8bccd
jaxp2 <spx0169x@gmail.com>
2022-08-10 20:37:46 +0100

    Update verify.py

--- cogs/verify.py
+++ cogs/verify.py
@@ -1,4 +1,3 @@
-from doctest import debug_src
 import discord
 from discord.ext import commands
 import random
@@ -16,7 +15,7 @@ class Verify(commands.Cog):
         elif member.guild.id == 1002659843720617995:
             welcomeid = 1002659844353970320
         await self.bot.get_channel(welcomeid).send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description=f"Welcome to the {member.guild.name}! Please read the rules. If you have any questions, please ask a staff member. Have fun!", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif"))
-        await member.send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description=f"Welcome to the {member.guild.name}! Please read the rules. Please verify yourself with the verification command `>verify {self.key}`. Have fun!", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif"))
+        await member.send(f"<@{member.id}>",embed=discord.Embed(title=f":wave: Welcome to {member.guild.name}!", description=f"Welcome to the {member.guild.name}! Please verify yourself with the verification command `>verify {self.key}`.", color=discord.Color.blue()).set_footer(text=f'Welcome {member.name}!', icon_url=member.avatar_url).set_image(url="https://i.imgur.com/pkWTMMl.gif"))
     @commands.command(name='verify', aliases=['v'])
     async def verify(self, ctx, key):
         await ctx.message.delete()
@@ -29,7 +28,7 @@ class Verify(commands.Cog):
           return
         if str(self.key) == key:
             await ctx.author.add_roles(verifyRole)
-            await ctx.author.send(f"<@{ctx.author.id}>", embed=discord.Embed(description="You have been verified!", color=discord.Color.red(), delete_after=60).set_footer(text=f"Requested by {ctx.author.name} | This message will automatically delete itself after 60 seconds.", icon_url=ctx.author.avatar_url).set_author(name="Verified", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/checkmark-green.png?token=GHSAT0AAAAAABV3GFY562BKGYAAD6XVUZCYYXSUEMA"))
+            await ctx.author.send(f"<@{ctx.author.id}>", embed=discord.Embed(description="You have been verified!", color=discord.Color.green(), delete_after=60).set_footer(text=f"Requested by {ctx.author.name} | This message will automatically delete itself after 60 seconds.", icon_url=ctx.author.avatar_url).set_author(name="Verified", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/checkmark-green.png?token=GHSAT0AAAAAABV3GFY562BKGYAAD6XVUZCYYXSUEMA"))
         else:
             await ctx.author.send(f"<@{ctx.author.id}>", embed=discord.Embed(description="Invalid key. Please try again.", color=discord.Color.red(), delete_after=60).set_footer(text=f"Requested by {ctx.author.name} | This message will automatically delete itself after 60 seconds.", icon_url=ctx.author.avatar_url).set_author(name="Error", icon_url="https://raw.githubusercontent.com/CriticRay/anix-images/main/crossmark-red.png?token=GHSAT0AAAAAABV3GFY4J4T3G6OGZTZLGGXQYXOQENQ"))
             self.key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(16))
